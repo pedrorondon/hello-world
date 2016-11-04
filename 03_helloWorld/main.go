955b91fd0f35c2bcac71d1fe3bb99a1232c10854
Pedro Rondon <pedro@blinkanalytics.com>
2016-11-04 14:42:22 -0400

    new stuff

---
+++ 03_helloWorld/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("fuck ")
+	fmt.Print("you ")
+	fmt.Println("asshole")
+}
---
+++ 04_helloWorld/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/everdev/mack"
+
+func main() {
+	mack.Beep(2)
+	mack.Say("this is so awesome")
+	mack.Say("over and out")
+	mack.Beep(3)
+}
